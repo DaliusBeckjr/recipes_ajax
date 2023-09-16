b6ab71b8f2ca3b0176e8e7aca906f843399cf83b
Dalius Beck <daliusbeckxiv@gmail.com>
2023-09-15 18:04:51 -0700

    Update user.py

--- flask_app/models/user.py
+++ flask_app/models/user.py
@@ -24,8 +24,7 @@ class User:
 # create
     @classmethod 
     def save(cls, data):
-        query = """ 
-            INSERT INTO users
+        query = """ INSERT INTO users
             (first_name, last_name, email, password)
             VALUES (%(first_name)s, %(last_name)s, %(email)s, %(password)s);
         """
